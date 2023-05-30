d40402b1537a0ef2e311753e12da291a000d163d
Srijan Kumar Khan <srijan.khan20@gmail.com>
2023-05-30 17:43:54 +0530

    Added TypedJS

--- assets/js/main.js
+++ assets/js/main.js
@@ -189,3 +189,12 @@ sr.reveal(`.skills__content:nth-child(2),.contact__content:nth-child(2)`, {
   origin: "right",
 });
 sr.reveal(`.qualification__content`, { interval: 100 });
+
+/*=============== TYPED JS ===============*/
+var typed = new Typed(".text", {
+  strings: ["Frontend Developer", "Backend Developer", "Web Developer"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
+});
